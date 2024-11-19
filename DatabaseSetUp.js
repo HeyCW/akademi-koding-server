a21cbd22d23127a169ff4cc20f959d260f5fef62
Nicholas Fransisco <nicholasfransisco09@gmail.com>
2024-11-19 15:33:00 +0700

    rip english

--- DatabaseSetUp.js
+++ DatabaseSetUp.js
@@ -132,7 +132,7 @@ function createTable() {
         user_id INT NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(id),
         link TEXT NOT NULL,
-        coment TEXT,
+        comment TEXT,
         score INT
     );
     `;
