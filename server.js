a3641a7e560172cfadfcd7cc62de0a11baf46d23
HeyCW <131667818+HeyCW@users.noreply.github.com>
2024-11-22 01:35:22 +0700

    all option

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.options('*', cors());
 
 database.createTable();
 
